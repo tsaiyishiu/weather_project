08db399935fb4612ea8943830ed94995bf025ad7
yishiu <zxc86655050zxc@gmail.com>
2022-06-11 15:15:12 +0800

    切到weather-gradient漸層

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main d-flex">
+      <div class="container d-flex">
+        <div class="wathercontent_today flex-fill">
+          <div class="weather-gradient"></div>
+        </div>
+        <div class="wathercontent flex-fill">
+
+        </div>
+      </div>
+    </div>
+    
   </div>
+
+  <!--
+    1.分頁 轉換頁面 路由器router （vue router後續）
+    2.首頁漂亮練切版，天氣預報為主題 皆天氣預報api
+    3.副頁欄位 輸入名字電話信箱地址 送入localstrogi
+    
+    
+   -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+       
+      }
+    },
+     methods: {
+      
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    max-width: 1440px;
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(61, 50, 57);
+    margin: auto;
+  }
+  .main{
+    height:100vh;
+  }
+  .container{
+    align-items: center;
+    justify-content: center;
+  }
+  .wathercontent{
+    max-width: 100%;
+    height: 500px;
+    background:rgb(25, 19, 23);
+    border-radius: 0 10% 10% 0;
+    transform: translate(-40px, 0px);
+
+  }
+  .wathercontent_today{
+    background-image: url('@/assets/01.jpeg');
+    background-repeat:no-repeat;
+    background-size: cover;
+    max-width: 100%;
+    height: 500px;
+    border-radius: 50px;
+    z-index: 2;
+  }
+  .weather-gradient{
+    width: 100%;
+    height: 100%;
+    border-radius: 50px;
+    opacity: 0.8;
+    background: #FDC830;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to left, #F37335, #FDC830);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to left, #F37335, #FDC830); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  }
+  
+    
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
