c15cccac80aa5c2251673dbc995c61537e17f960
yishiu <zxc86655050zxc@gmail.com>
2022-06-13 20:19:37 +0800

    切版中info_today_content

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,29 @@
   <div id="app">
     <div class="main d-flex">
       <div class="container d-flex">
-        <div class="wathercontent_today flex-fill">
-          <div class="weather-gradient"></div>
+        <div class="wathercontent_today ">
+          <div class="weather-gradient">
+            <div class="date-container">
+              <h2 class="worship">Tuesday</h2>  
+              <h3 class="year month day">15 Jan 2022</h3>
+              <h3 class="Place">taipei</h3>
+            </div>
+            <div class="weather-container">
+              <i class="fa-solid fa-sun"></i>
+              <h3 class="temperature">29°C</h3>
+              <h3 class="weather-desc">weather-desc</h3>
+            </div> 
+          </div>
         </div>
-        <div class="wathercontent flex-fill">
-
+        <div class="info_today_content ">
+          <div class="info_today">
+            <div class="precipitation">
+              <span>precipitation</span>
+              <span>0%</span>
+            </div>
+            <div class=""></div>
+            <div class=""></div>
+          </div>
         </div>
       </div>
     </div>
@@ -50,8 +68,9 @@
     align-items: center;
     justify-content: center;
   }
-  .wathercontent{
-    max-width: 100%;
+  .info_today_content{
+    /* max-width: 100%; */
+    width: 500px;
     height: 500px;
     background:rgb(25, 19, 23);
     border-radius: 0 10% 10% 0;
@@ -62,7 +81,8 @@
     background-image: url('@/assets/01.jpeg');
     background-repeat:no-repeat;
     background-size: cover;
-    max-width: 100%;
+    width: 400px;
+    /* max-width: 100%; */
     height: 500px;
     border-radius: 50px;
     z-index: 2;
@@ -75,10 +95,19 @@
     background: #FDC830;  /* fallback for old browsers */
     background: -webkit-linear-gradient(to left, #F37335, #FDC830);  /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(to left, #F37335, #FDC830); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
   }
-  
-    
+  .date-container{
+    padding-left:20px ;
+    padding-top:20px ;
+  }
+  .weather-container{
+    padding-left:20px ;
+    padding-top:160px ;
+  }  
+  .fa-sun{
+    font-size: 50px;
+    padding-bottom: 10px;
+  }
 </style>
 
 
