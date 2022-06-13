628c3b0ed39c178921db61aef6bed29efbc0bd1d
yishiu <zxc86655050zxc@gmail.com>
2022-06-13 23:23:43 +0800

    架構整理引進引進scss

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <div id="app">
     <div class="main d-flex">
       <div class="container d-flex">
-        <div class="wathercontent_today ">
-          <div class="weather-gradient">
-            <div class="date-container">
+        <div class="container_left">
+          <div class="background_city">
+            <div class="Target">
               <h2 class="worship">Tuesday</h2>  
               <h3 class="year month day">15 Jan 2022</h3>
               <h3 class="Place">taipei</h3>
             </div>
-            <div class="weather-container">
+            <div class="current">
               <i class="fa-solid fa-sun"></i>
               <h3 class="temperature">29°C</h3>
               <h3 class="weather-desc">weather-desc</h3>
             </div> 
           </div>
         </div>
-        <div class="info_today_content ">
+        <div class="container_right ">
           <div class="info_today">
             <div class="precipitation">
               <span>precipitation</span>
@@ -24,6 +24,9 @@
             </div>
             <div class=""></div>
             <div class=""></div>
+            
+            
+            
           </div>
         </div>
       </div>
@@ -53,57 +56,63 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   #app{
     max-width: 1440px;
     width: 100%;
     height: 100vh;
     background-color: rgb(61, 50, 57);
     margin: auto;
+    .main{
+      height:100vh;
+      .container{
+      align-items: center;
+      justify-content: center;
+        .container_left{
+          background-image: url('@/assets/01.jpeg');
+          background-repeat:no-repeat;
+          background-size: cover;
+          width: 400px;
+          /* max-width: 100%; */
+          height: 500px;
+          border-radius: 50px;
+          z-index: 2;
+          .background_city{
+            width: 100%;
+            height: 100%;
+            border-radius: 50px;
+            opacity: 0.8;
+            background: #FDC830;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #F37335, #FDC830);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #F37335, #FDC830); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            .Target{
+              padding-left:20px ;
+              padding-top:20px ;
+            }
+            .current{
+              padding-left:20px ;
+              padding-top:160px ;
+            }  
+          }
+        }
+        .container_right{
+        /* max-width: 100%; */
+        width: 500px;
+        height: 500px;
+        background:rgb(25, 19, 23);
+        border-radius: 0 10% 10% 0;
+        transform: translate(-40px, 0px);
+        }
+      }
+    }
   }
-  .main{
-    height:100vh;
-  }
-  .container{
-    align-items: center;
-    justify-content: center;
-  }
-  .info_today_content{
-    /* max-width: 100%; */
-    width: 500px;
-    height: 500px;
-    background:rgb(25, 19, 23);
-    border-radius: 0 10% 10% 0;
-    transform: translate(-40px, 0px);
-
-  }
-  .wathercontent_today{
-    background-image: url('@/assets/01.jpeg');
-    background-repeat:no-repeat;
-    background-size: cover;
-    width: 400px;
-    /* max-width: 100%; */
-    height: 500px;
-    border-radius: 50px;
-    z-index: 2;
-  }
-  .weather-gradient{
-    width: 100%;
-    height: 100%;
-    border-radius: 50px;
-    opacity: 0.8;
-    background: #FDC830;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to left, #F37335, #FDC830);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to left, #F37335, #FDC830); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  }
-  .date-container{
-    padding-left:20px ;
-    padding-top:20px ;
-  }
-  .weather-container{
-    padding-left:20px ;
-    padding-top:160px ;
-  }  
+  
+  
+  
+  
+  
+  
+  
   .fa-sun{
     font-size: 50px;
     padding-bottom: 10px;
