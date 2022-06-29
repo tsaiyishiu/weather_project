bcc2e79bea4b96305579a80bc8b10a1eb52fa75a
yishiu <zxc86655050zxc@gmail.com>
2022-06-29 19:07:37 +0800

    拆componnner前存檔

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,53 @@
 <template>
   <div id="app">    
-    <div>
-      <b-button variant="outline-warning">留言</b-button>
-      <div class="main d-flex">
-        <div class="container d-flex align-items-center">
-          <div class="container_under d-flex ">
-            <div class="container_now">
-              <div class="background_city">
-                <div class="Target">
-                  <h2 class="worship">{{ howDay }}</h2>  
-                  <h3 class="year_month_day">{{ yearMonthDay }}</h3>
-                  <h3 class="place">{{ howPlace }}</h3>
-                </div>
-                <div class="current">
-                  <div class="icon" v-html="icon"></div>
-                  <h3 class="temperature">{{ howTemperature }} &#8451;</h3>
-                  <h3 class="weather-desc">{{ howWeather }}</h3>
-                </div> 
+    <b-button variant="outline-warning">留言</b-button>
+    <div class="main d-flex">
+      <!-- <h1>天氣預報</h1> -->
+      <div class="container d-flex align-items-center">
+        <div class="container_under d-flex ">
+          <div class="container_now">
+            <div class="background_city">
+              <div class="Target">
+                <h2 class="worship">{{ howDay }}</h2>  
+                <h3 class="year_month_day">{{ yearMonthDay }}</h3>
+                <h3 class="place">{{ howPlace }}</h3>
+              </div>
+              <div class="current">
+                <div class="icon" v-html="icon"></div>
+                <h3 class="temperature">{{ howTemperature }} &#8451;</h3>
+                <h3 class="weather-desc">{{ howWeather }}</h3>
+              </div> 
+            </div>
+          </div>
+          <div class="container_future ">
+            <div class="info_current d-flex flex-column">
+              <div v-for="(item, index) in weatherInformation" :key='"item"+index' class="precipitation">
+                <span>{{ item.message }}</span>
+                <span>{{ item.messageData }}{{ item.messageunit}}</span>
               </div>
             </div>
-            <div class="container_future ">
-              <div class="info_current d-flex flex-column">
-                <div v-for="item in items" :key='item.message' class="precipitation">
-                  <span>{{ item.message }}</span>
-                  <span>{{ item.messageData }}{{ item.messageunit}}</span>
+            <div class="forecast_weather">
+              <div class="forecast_items d-flex justify-content-center">
+                <div
+                  v-for="(futureitem, index) in futureDays" :key='"futureitem"+index'
+                  class="dayone d-flex flex-column col-3 justify-content-around"
+                >
+                  <div v-html="futureitem.messageIcon"></div>
+                  <span>{{ futureitem.messageWeek }}</span>
+                  <span>{{ futureitem.messageTemperature }}°C</span>
                 </div>
               </div>
-              <div class="forecast_weather">
-                <div class="forecast_items d-flex justify-content-center">
-                  <div
-                    v-for="(futureitem, index) in futureitems" :key='"futureitem"+index'
-                    class="dayone d-flex flex-column col-3 justify-content-around"
-                  >
-                    <div v-html="futureitem.messageIcon"></div>
-                    <span>{{ futureitem.messageWeek }}</span>
-                    <span>{{ futureitem.messageTemperature }}°C</span>
-                  </div>
-                </div>
-              </div>
-              <div class="place_items">
-                <button class="place_item">
-                    <i class="fa-solid fa-location-dot"></i>
-                    <span>Change location</span>
-                </button>
-              </div>
-            </div>  
-          </div>
+            </div>
+            <div class="place_items">
+              <button class="place_item">
+                  <i class="fa-solid fa-location-dot"></i>
+                  <span>Change location</span>
+              </button>
+            </div>
+          </div>  
         </div>
       </div>
-    </div>  
+    </div>
   </div>
   
 
@@ -69,8 +68,8 @@
   export default {
     mounted(){
       this.getApi()
-      this.day()
-      this.yearMonthDays()
+      this.showWeek()
+      this.showYearMonthDays()
     },
     data() {
       return {
@@ -80,7 +79,7 @@
         howTemperature:'',
         howWeather:'',
         icon:'',
-        items:[
+        weatherInformation:[
           {message:'precipitation',
            messageData:'???',
            messageunit:'%'
@@ -94,7 +93,7 @@
            messageunit:'km/h',
           },
         ],
-        futureitems:[
+        futureDays:[
           {messageIcon:'',
            messageWeek:'Three',
            messageTemperature:'29',
@@ -115,20 +114,20 @@
         info : null,
         weekDay: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
         monthDay: ['Jan','Feb','Mar','Apr','May','Jun','Aug','Sept','Oct','Nov','Dec'],
-        futureDay: ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'],
-        city:[]
+        weekAbbreviation: ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'],
+        cites:[]
       }
     },
     methods: {
       /**  顯示星期 */
-      day: function() {   
+      showWeek: function() {   
         const birthday = new Date();
         const day1 = birthday.getDay();
-        this.futureday(day1)
+        this.showFutureWeek(day1)
         this.howDay = this.weekDay[day1]
       },
       /** 顯示未來星期 */
-      futureday: function(day1) {
+      showFutureWeek: function(day1) {
         // let day2 = day1 +1
         // if(day2 >= 7){
         //   day2 = day2 - 7
@@ -145,61 +144,61 @@
         // if(day5 >= 7){
         //   day5 = day5 - 7
         // }
-        // this.futureitems[0].messageWeek = this.futureDay[day2]
-        // this.futureitems[1].messageWeek = this.futureDay[day3] 
-        // this.futureitems[2].messageWeek = this.futureDay[day4]
-        // this.futureitems[3].messageWeek = this.futureDay[day5]
-          for (let index = 0; index < 4; index++) {
+        // this.futureDays[0].messageWeek = this.weekAbbreviation[day2]
+        // this.futureDays[1].messageWeek = this.weekAbbreviation[day3] 
+        // this.futureDays[2].messageWeek = this.weekAbbreviation[day4]
+        // this.futureDays[3].messageWeek = this.weekAbbreviation[day5]
+        for (let index = 0; index < 4; index++) {
           let newDay = day1 + (index+1)
           if(newDay >= 7){
             newDay = newDay - 7
           }
-          this.futureitems[index].messageWeek = this.futureDay[newDay]
+          this.futureDays[index].messageWeek = this.weekAbbreviation[newDay]
         }
       },
       /** 顯示年月日 */
-      yearMonthDays: function() { 
+      showYearMonthDays: function() { 
         const myDate = new Date(); 
-        const yearMonThday = myDate.getFullYear()+' '+ this.monthDay[myDate.getMonth()] +' '+myDate.getDate()
-        this.yearMonthDay = yearMonThday
+        const yearMonthDay = myDate.getFullYear()+' '+ this.monthDay[myDate.getMonth()] +' '+myDate.getDate()
+        this.yearMonthDay = yearMonthDay
       },
       getApi: function() {
         this.axios
         .get ( 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-093?Authorization=CWB-CEAB4C1A-D854-4B57-BB0E-0ACFB2760821&locationId=F-D0047-063' ) 
         .then ( response => { 
           console.log(response)
-          this.cityApi(response)
-        } ) 
+          this.showCity(response)
+        }) 
         .catch ( 
-        function ( error ) { //請求失敗處理   
-        console . log ( error ) ;
-        } ) 
+          function ( error ) { //請求失敗處理   
+          console.log( error );
+        }) 
         
       },
       /** 顯示地區 */
-      cityApi(response){ 
-        const citys = response.data.records.locations[0].location 
-        this.Temperature(citys)
-        this.Weather(citys)
-        this.precipitation(citys)
-        this.humidity(citys)
-        this.wind(citys)
-        this.futureTemperature(citys)
-        this.futureIcon(citys)
-        this.weatherIcon(citys)
-        const city =  citys[7].locationName
-        this.howPlace = city 
+      showCity(response){ 
+        const city = response.data.records.locations[0].location 
+        this.showTemperature(city)
+        this.showWeather(city)
+        this.showPrecipitation(city)
+        this.showHumidity(city)
+        this.showWind(city)
+        this.showFutureTemperature(city)
+        this.showFutureIcon(city)
+        this.showWeatherIcon(city)
+        const cites = city[7].locationName
+        this.howPlace = cites
       },
       /** 顯示溫度 */
-      Temperature: function(citys){
-        const temperatures = citys[7].weatherElement[1].time[0].elementValue[0].value
+      showTemperature: function(city){
+        const temperatures = city[7].weatherElement[1].time[0].elementValue[0].value
         this.howTemperature = temperatures
         //console.log(temperatures)
       },
       /** 顯示天氣圖示 */
-      weatherIcon:function(citys){
+      showWeatherIcon: function(city){
         for (let index = 0; index < 4; index++) {
-          let weatherIcons = Number(citys[7].weatherElement[6].time[0].elementValue[1].value)  
+          let weatherIcons = Number(city[7].weatherElement[6].time[0].elementValue[1].value)  
           if (weatherIcons <= 4){
             weatherIcons = '<i class="fa-solid fa-sun "></i>'
           }
@@ -222,38 +221,39 @@
         }
       },
       /** 顯示天氣 */
-      Weather: function(citys){
-        const howWeathers = citys[7].weatherElement[6].time[0].elementValue[0].value
+      showWeather: function(city){
+        const howWeathers = city[7].weatherElement[6].time[0].elementValue[0].value
         this.howWeather = howWeathers 
       },
       /** 顯示降雨率 */
-      precipitation: function(citys){ 
-        const cipitations = citys[7].weatherElement[0].time[0].elementValue[0].value
-        this.items[0].messageData = cipitations + ' '
+      showPrecipitation: function(city){ 
+        const cipitations = city[7].weatherElement[0].time[0].elementValue[0].value
+        this.weatherInformation[0].messageData = cipitations + ' '
       },
       /** 顯示濕度 */
-      humidity: function(citys){ 
-        const humiditys = citys[7].weatherElement[2].time[0].elementValue[0].value
-        this.items[1].messageData = humiditys+ ' '
+      showHumidity: function(city){ 
+        const humiditys = city[7].weatherElement[2].time[0].elementValue[0].value
+        this.weatherInformation[1].messageData = humiditys+ ' '
       },
       /** 顯示風速 */
-      wind: function(citys){ 
-        const winds = citys[7].weatherElement[4].time[0].elementValue[0].value
-        this.items[2].messageData = winds * 3600 / 1000 + ' '
+      showWind: function(city){ 
+        const winds = city[7].weatherElement[4].time[0].elementValue[0].value
+        this.weatherInformation[2].messageData = winds * 3600 / 1000 + ' '
       },
       /**顯示未來溫度 */
-      futureTemperature:function(citys){
-        const futureTemperaturesOne = citys[7].weatherElement[1].time[2].elementValue[0].value
-        const futureTemperaturesTwo = citys[7].weatherElement[1].time[4].elementValue[0].value
-        const futureTemperaturesThree = citys[7].weatherElement[1].time[6].elementValue[0].value
-        const futureTemperaturesFour = citys[7].weatherElement[1].time[8].elementValue[0].value
-        this.futureitems[0].messageTemperature = futureTemperaturesOne
-        this.futureitems[1].messageTemperature = futureTemperaturesTwo
-        this.futureitems[2].messageTemperature = futureTemperaturesThree
-        this.futureitems[3].messageTemperature = futureTemperaturesFour
+      showFutureTemperature: function(city){
+        const futureTemperaturesOne = city[7].weatherElement[1].time[2].elementValue[0].value
+        const futureTemperaturesTwo = city[7].weatherElement[1].time[4].elementValue[0].value
+        const futureTemperaturesThree = city[7].weatherElement[1].time[6].elementValue[0].value
+        const futureTemperaturesFour = city[7].weatherElement[1].time[8].elementValue[0].value
+        this.futureDays[0].messageTemperature = futureTemperaturesOne
+        this.futureDays[1].messageTemperature = futureTemperaturesTwo
+        this.futureDays[2].messageTemperature = futureTemperaturesThree
+        this.futureDays[3].messageTemperature = futureTemperaturesFour
       },
-      futureIcon:function(citys){
-        // let futureIconOne = Number(citys[7].weatherElement[6].time[2].elementValue[1].value)
+      /** 顯示未來天氣圖示 */
+      showFutureIcon: function(city){
+        // let futureIconOne = Number(city[7].weatherElement[6].time[2].elementValue[1].value)
         // console.log(futureIconOne)
         
         // if (futureIconOne <= 4){
@@ -276,8 +276,8 @@
         // if( 41 >= futureIconOne >= 29){
         //   futureIconOne = '<i class="fa-solid fa-cloud-rain"></i> '
         // }
-        // this.futureitems[0].messageIcon = futureIconOne
-        // let futureIconTwo = Number(citys[7].weatherElement[6].time[4].elementValue[1].value)
+        // this.futureDays[0].messageIcon = futureIconOne
+        // let futureIconTwo = Number(city[7].weatherElement[6].time[4].elementValue[1].value)
         // if (futureIconTwo <= 4){
           
         //   futureIconTwo = '<i class="fa-solid fa-sun "></i>'
@@ -298,8 +298,8 @@
         // if( 41 >= futureIconTwo >= 29){
         //   futureIconTwo = '<i class="fa-solid fa-cloud-rain"></i> '
         // }
-        // this.futureitems[1].messageIcon = futureIconTwo
-        // let futureIconThree = Number(citys[7].weatherElement[6].time[6].elementValue[1].value)
+        // this.futureDays[1].messageIcon = futureIconTwo
+        // let futureIconThree = Number(city[7].weatherElement[6].time[6].elementValue[1].value)
         // if (futureIconThree <= 4){
           
         //   futureIconThree = '<i class="fa-solid fa-sun "></i>'
@@ -320,8 +320,8 @@
         // if( 41 >= futureIconThree >= 29){
         //   futureIconThree = '<i class="fa-solid fa-cloud-rain"></i> '
         // }
-        // this.futureitems[2].messageIcon = futureIconThree
-        // let futureIconFour = Number(citys[7].weatherElement[6].time[8].elementValue[1].value)
+        // this.futureDays[2].messageIcon = futureIconThree
+        // let futureIconFour = Number(city[7].weatherElement[6].time[8].elementValue[1].value)
         // if (futureIconFour <= 4){
           
         //   futureIconFour = '<i class="fa-solid fa-sun "></i>'
@@ -342,9 +342,9 @@
         // if( 41 >= futureIconFour >= 29){
         //   futureIconFour = '<i class="fa-solid fa-cloud-rain"></i> '
         // }
-        // this.futureitems[3].messageIcon = futureIconFour
+        // this.futureDays[3].messageIcon = futureIconFour
         for (let index = 0; index < 4; index++) {
-          let futureIconOne = Number(citys[7].weatherElement[6].time[(index+1)*2].elementValue[1].value)
+          let futureIconOne = Number(city[7].weatherElement[6].time[(index+1)*2].elementValue[1].value)
           if (futureIconOne <= 4){
             futureIconOne = '<i class="fa-solid fa-sun "></i>'
           }
@@ -364,15 +364,12 @@
           if( 41 >= futureIconOne && futureIconOne >= 29){
             futureIconOne = '<i class="fa-solid fa-cloud-rain"></i> '
           }
-          this.futureitems[index].messageIcon = futureIconOne
+          this.futureDays[index].messageIcon = futureIconOne
         }
       },
     },
     
   }
-
-
-  
 </script>
 
 <style lang="scss">
@@ -387,8 +384,8 @@
     .main{
       height:100vh;
       .container{
-      align-items: center;
-      justify-content: center;
+        align-items: center;
+        justify-content: center;
         .container_now{
           background-image: url('@/assets/01.jpeg');
           background-repeat:no-repeat;
